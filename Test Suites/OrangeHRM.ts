f80376d6dbfa390b999033356837234ed2c36dc6
lakshmi_kamal <lakshmikamal2018@outlook.com>
2023-01-12 21:39:37 -0500

    Signed-off-by: lakshmi_kamal <lakshmikamal2018@outlook.com>

---
+++ Test Suites/OrangeHRM.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>OrangeHRM</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>ee0bc3cf-dd18-4c92-a6a8-3be1522ffd03</testSuiteGuid>
+   <testCaseLink>
+      <guid>114e5177-3598-4ffa-bd0e-0b8836ac4725</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/OrangeHRM/Login and Logout</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
