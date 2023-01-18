fbeb7a8cf6d08eb2673f626975452d8b2ca1d493
lakshmi_kamal <lakshmikamal2018@outlook.com>
2023-01-17 19:31:35 -0500

    commit 3

--- Scripts/OrangeHRM/Login and Logout/Script1673577471057.groovy
+++ Scripts/OrangeHRM/Login and Logout/Script1673577471057.groovy
@@ -33,3 +33,34 @@ WebUI.delay(1)
 
 WebUI.click(findTestObject('Object Repository/Page_OrangeHRM/a_Logout'))
 
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://katalon-demo-cura.herokuapp.com/')
+
+WebUI.click(findTestObject('Object Repository/CURA Healthcare/Page_CURA Healthcare Service/a_Make Appointment'))
+
+WebUI.setText(findTestObject('CURA Healthcare/Page_CURA Healthcare Service/input_Username_username'), 'John Doe')
+
+WebUI.setEncryptedText(findTestObject('Object Repository/CURA Healthcare/Page_CURA Healthcare Service/input_Password_password'), 
+    'g3/DOGG74jC3Flrr3yH+3D/yKbOqqUNM')
+
+WebUI.click(findTestObject('Object Repository/CURA Healthcare/Page_CURA Healthcare Service/button_Login'))
+
+WebUI.selectOptionByValue(findTestObject('Object Repository/CURA Healthcare/Page_CURA Healthcare Service/select_Tokyo CURA Healthcare Center        _5b4107'), 
+    'Hongkong CURA Healthcare Center', true)
+
+WebUI.click(findTestObject('Object Repository/CURA Healthcare/Page_CURA Healthcare Service/input_Apply for hospital readmission_hospit_63901f'))
+
+WebUI.click(findTestObject('Object Repository/CURA Healthcare/Page_CURA Healthcare Service/input_Medicaid_programs'))
+
+WebUI.click(findTestObject('Object Repository/CURA Healthcare/Page_CURA Healthcare Service/div_Visit Date (Required)_input-group-addon'))
+
+WebUI.click(findTestObject('Object Repository/CURA Healthcare/Page_CURA Healthcare Service/td_18'))
+
+WebUI.setText(findTestObject('Object Repository/CURA Healthcare/Page_CURA Healthcare Service/textarea_Comment_comment'), 
+    'test')
+
+WebUI.click(findTestObject('Object Repository/CURA Healthcare/Page_CURA Healthcare Service/button_Book Appointment'))
+
+WebUI.click(findTestObject('Object Repository/CURA Healthcare/Page_CURA Healthcare Service/a_Go to Homepage'))
+
